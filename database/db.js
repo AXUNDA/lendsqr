fe34887ef2664c3599be3be5614aa15809be0657
Axunda <charlesemmanuelazunda@gmail.com>
2022-11-07 10:03:35 +0100

    removed db admin details

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
       client: 'mysql2',
       connection: {
-        host : 'containers-us-west-87.railway.app',
-        port : 5635,
-        user : 'root',
-        password : 'E8EtqIJ3h9lwcnfpw26F',
-        database : 'railway'
+        host : '',
+        port : ,
+        user : '',
+        password : '',
+        database : ''
         
       }
     });
